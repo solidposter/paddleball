a8be5749fe562756dc576f874abe3ae36f938c5f
Tony Sarendal <tony@polarcap.org>
2019-03-14 05:50:21 +0100

    Add algorithm to find re-order and drops.

--- statsengine.go
+++ statsengine.go
@@ -23,10 +23,10 @@ import (
 
 var pslice1 = []payload{}	// live data slice, data is fed here
 var pslice2 = []payload{}	// old data slice, analysis is done here
-var badname map[int64]payload
+var serMap map[int64]int64	// expected serial number, key = client ID
 
 func statsengine(rp <-chan payload, rate int, numclients int) {
-	badname = make(map[int64]payload)
+	serMap = make(map[int64]int64)
 
 	ticker := time.NewTicker(time.Second)
 	message := payload{}
@@ -46,16 +46,44 @@ func statsengine(rp <-chan payload, rate int, numclients int) {
 
 
 func process() {
-	for _,v := range pslice2 {	// process old data
-		elem, ok := badname[v.Id]
+	for i,message := range pslice2 {	// process old data
+		nser, ok := serMap[message.Id]
 		if ok {
-			if v.Serial == elem.Serial+1 {
-				fmt.Println("correct packet order:", v.Id, elem.Serial, v.Serial)
-			} else { 
-				fmt.Println("wrong packet order:", v.Id, elem.Serial, v.Serial)
+			if message.Serial == nser {
+				fmt.Println("correct packet order:", message.Id, nser, message.Serial)
+				serMap[message.Id] = message.Serial+1
+			} else if message.Serial >  nser { 
+				if findPacket(i, message.Id) {
+					fmt.Println("packet re-order:", message.Id, nser, message.Serial)
+				} else {
+					fmt.Println("packet loss:",message.Id, nser)
+				}
+				serMap[message.Id] = message.Serial+2
+			} else {
+				continue	// lower than expected serial
+			}
+		} else {
+			serMap[message.Id] = message.Serial+1	// first packet seen for this client ID
+		}
+	}
+}
+
+func findPacket(pos int, id int64) bool {
+	for _,v := range pslice2[pos:] {
+		if v.Id == id {
+			if v.Serial == serMap[v.Id] {
+				return true	// packet found, re-order
 			}
 		}
-		badname[v.Id] = v
 	}
+
+	for _,v := range pslice1 {	// search the next slice also
+		if v.Id == id {
+			if v.Serial == serMap[v.Id] {
+				return true	// packet found, re-order
+			}
+		}
+	}
+	return false	// packet not found, drop
 }
 

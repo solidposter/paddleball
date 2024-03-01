7dae8a571a2af2699f343385d8eae8bd52efb086
Tony Sarendal <tony.sarendal@kindredgroup.com>
2024-03-01 06:44:49 +0100

    Reduces size of output

--- structs.go
+++ structs.go
@@ -9,33 +9,33 @@ type packetStats struct {
 }
 
 type report struct {
-	Tag                   string
-	ReceivedPackets       int64
-	DroppedPackets        int64
-	DroppedPercent        float64
-	DuplicatePackets      int64
-	ReorderedPackets      int64
-	ReorderedPercent      float64
-	AverageRTT            time.Duration
-	LowestRTT             time.Duration
-	HighestRTT            time.Duration
-	PBQueueDroppedPackets int64
-	PBQueueLength         int
-	PBQueueCapacity       int
+	Tag      string
+	Received int64
+	Dropped  int64
+	//DroppedPercent        float64
+	Duplicates int64
+	Reordered  int64
+	//ReorderedPercent      float64
+	AverageRTT      time.Duration
+	LowestRTT       time.Duration
+	HighestRTT      time.Duration
+	PBQueueDropped  int64
+	PBQueueLength   int
+	PBQueueCapacity int
 }
 
 // provides an incomplete report (missing tag and queue lengths)
 func (s packetStats) Report() report {
 	var r report
-	r.ReceivedPackets = s.rcvdPkts
-	r.DroppedPackets = s.dropPkts
-	r.DroppedPercent = float64(s.dropPkts) / float64(s.rcvdPkts+s.dropPkts) * 100
-	r.DuplicatePackets = s.dupPkts
-	r.ReorderedPackets = s.reordPkts
-	r.ReorderedPercent = float64(s.reordPkts) / float64(s.rcvdPkts+s.dropPkts) * 100
+	r.Received = s.rcvdPkts
+	r.Dropped = s.dropPkts
+	//r.DroppedPercent = float64(s.dropPkts) / float64(s.rcvdPkts+s.dropPkts) * 100
+	r.Duplicates = s.dupPkts
+	r.Reordered = s.reordPkts
+	//r.ReorderedPercent = float64(s.reordPkts) / float64(s.rcvdPkts+s.dropPkts) * 100
 	r.LowestRTT = s.minRtt
 	r.HighestRTT = s.maxRtt
 	r.AverageRTT = time.Duration(float64(s.totRtt) / float64(s.rcvdPkts))
-	r.PBQueueDroppedPackets = s.pbdropPkts
+	r.PBQueueDropped = s.pbdropPkts
 	return r
 }
